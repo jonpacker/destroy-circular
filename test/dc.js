536c1344817aabb821802b7975bd341794eead91
Jon Packer <contact@jonpacker.com>
2014-08-05 15:14:52 +0200

    add test using arrays

--- test/dc.js
+++ test/dc.js
@@ -31,4 +31,20 @@ describe('destroy-circular', function() {
     assert.equal(d.one.firstThing.thing, '[Circular]')
     assert.equal(d.two.secondThing.thing, '[Circular]')
   });
+  it('should work on arrays', function() {
+    var obj = {};
+    var common = [obj];
+    var x = [common];
+    var y = [['test'], common];
+    y[0][1] = y;
+    obj.a = {x:x};
+    obj.b = {y:y};
+
+    var d = dc(obj);
+    assert(Array.isArray(d.a.x));
+    assert.equal(d.a.x[0][0], '[Circular]');
+    assert.equal(d.b.y[0][0], 'test');
+    assert.equal(d.b.y[1][0], '[Circular]');
+    assert.equal(d.b.y[0][1], '[Circular]');
+  });
 })
