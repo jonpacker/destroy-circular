1b0ad42ec24a76353c6b73e9fa4f6a4606a14d79
Jon Packer <contact@jonpacker.com>
2014-08-05 15:09:33 +0200

    rewrite implementation

--- dc.js
+++ dc.js
@@ -1,2 +1,15 @@
-var circular = require('circular');
-module.exports = function(obj) { return JSON.parse(JSON.stringify(obj, circular())) }
+module.exports = function(obj) {
+  function copy(from, seen) {
+    var to = {};
+    seen.push(from);
+    Object.keys(from).forEach(function(key) {
+      if (!from[key] || (typeof from[key] != 'object' && !Array.isArray(from[key]))) 
+        to[key] = from[key];
+      else if (seen.indexOf(from[key]) == -1) {
+        to[key] = copy(from[key], seen.slice(0));
+      } else to[key] = '[Circular]';
+    });
+    return to;
+  };
+  return copy(obj, []);
+};
